a4d7cf367f256752fb8b56be6b3692d7fac56f9f
Clemchowdah <imclemchowder@gmail.com>
2024-08-22 13:55:10 -0700

    and not statement

--- Loan Eligibility.py
+++ Loan Eligibility.py
@@ -1,7 +1,7 @@
 has_high_income=True
 has_good_credit=True
 
-if has_high_income and has_good_credit:
+if has_high_income and has_good_credit: #you can say and not if you want to exclude something, say not criminal record
     print("You are eligible for a loan.")
 
 else:
