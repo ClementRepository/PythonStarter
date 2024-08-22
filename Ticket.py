87870b8fdbf444326d3c903649e31e98cd7193d5
Clemchowdah <imclemchowder@gmail.com>
2024-08-22 13:19:27 -0700

    If Else ticket

---
+++ Ticket.py
@@ -0,0 +1,9 @@
+
+age = 26
+
+if age < 13:
+    print("You are a child.")
+elif age <= 24: #Elif means if else, which means more than one condition must be met.
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
