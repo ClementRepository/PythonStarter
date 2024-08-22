ef0364f42a8673ce9b1b7755b14bb23fefff0988
Clemchowdah <imclemchowder@gmail.com>
2024-08-22 13:40:45 -0700

    testscore3

--- Testscores.py
+++ Testscores.py
@@ -2,4 +2,4 @@ English=82
 Math=80
 Physics=77
 
-print(f"Clem got the total score of {English + Math + Physics} out of 300 points.")
+print(f"Clem scored {English + Math + Physics} out of 300 points.")
