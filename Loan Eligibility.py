b93acde4b8a5c477029e806a2e9c9c4b77972395
Clemchowdah <imclemchowder@gmail.com>
2024-08-22 13:51:49 -0700

    High income and statement

---
+++ Loan Eligibility.py
@@ -0,0 +1,8 @@
+has_high_income=True
+has_good_credit=True
+
+if has_high_income and has_good_credit:
+    print("You are eligible for a loan.")
+
+else:
+    print("You are not eligible for a loan.")
