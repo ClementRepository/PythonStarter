7a0006f052da8e96f3e1cc582d99dde93c004403
Clemchowdah <imclemchowder@gmail.com>
2024-08-22 13:41:39 -0700

    f string2

--- Testscores.py
+++ Testscores.py
@@ -2,4 +2,4 @@ English=82
 Math=80
 Physics=77
 
-print(f"Clem scored {English + Math + Physics} out of 300 points.")
+print(f"Clem scored {English + Math + Physics} out of 300 points.") #f string embeds another value in there.
