3699c92a8145e75f3799d937faea51ea1fecc304
Clemchowdah <imclemchowder@gmail.com>
2024-08-22 13:00:48 -0700

    MAFF

---
+++ maff.py
@@ -0,0 +1,10 @@
+import math #bring in math modules to use, make sure you're googling python3 math module https://docs.python.org/3/library/math.html
+math.ceil(2.) #high point of 2. whatever, equals 3
+math.floor(2.) #low point of 2. whatever, equals 2
+
+x=2.8
+print(round(x))
+print(abs(-2.8)) #abs gives absolute positive value
+
+print(math.ceil(2.8))
+print(math.floor(2.8))
