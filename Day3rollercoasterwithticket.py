5848fb210c74f1edf6dd41f7b2a5990833cf7c6f
Clemchowdah <imclemchowder@gmail.com>
2024-09-05 14:44:34 -0700

    fixed roller coaster

--- Day3rollercoasterwithticket.py
+++ Day3rollercoasterwithticket.py
@@ -11,7 +11,7 @@ if height >= 120:
         bill = 7
         print("Youth tickets are $7.")
     else:
-        print("Adults $12.")
+        print("Adult tickets are $12.")
         bill = 12
     wants_photo = input("Do you want a photo taken? Type y for Yes and n for No. ")
     if wants_photo == "y":
